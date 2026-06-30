3ba647ebf2ab60dee09e26587a487c170c6bf235
anoushka1405 <anoushka142006@gmail.com>
2026-06-30 15:31:58 +0530

    formatting

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
-import os
 from dotenv import load_dotenv
 from google import genai
+import os
 
 load_dotenv()
 
-client = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
+print("SDK loaded")
+print("Key starts with:", os.getenv("GEMINI_API_KEY")[:4])
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Say hello in one sentence."
+    contents="Say hello."
 )
 
 print(response.text)
